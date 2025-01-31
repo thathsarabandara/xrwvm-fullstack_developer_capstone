37102659880f811eeddfc65a0c321d6a191b90a7
thathsarabandara <thathsaraarumapperuma@gmail.com>
2025-01-31 16:06:38 +0530

    linting6

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': (
-            'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
         ),
     },
     {
@@ -147,4 +147,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
